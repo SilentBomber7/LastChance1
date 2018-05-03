94c2a2ffaa70b81ac579d355785a5ec32ca5f81c
SilentBomber7 <ace71182@gmail.com>
2018-05-02 20:43:28 -0500

    Assignment 7 update

--- FinalAssignment7/Data/DbInitializer.cs
+++ FinalAssignment7/Data/DbInitializer.cs
@@ -22,6 +22,7 @@ namespace FinalAssignment7.Data
             new User{Email="Bpitt@gmail.com",Name="Brad Pitt",CreatedOn=DateTime.Parse("2015-01-15")},
             new User{Email="RDJ@ironman.com",Name="Robert Downey Jr",CreatedOn=DateTime.Parse("2018-03-25")},
             new User{Email="AnthonyP@yahoo.com",Name="Anthony Perkins",CreatedOn=DateTime.Parse("2017-08-07")}
+            
             };
             foreach (User u in users)
             {
@@ -31,9 +32,10 @@ namespace FinalAssignment7.Data
 
             var categories = new Category[]
             {
-            new Category{Name="Physical Science"},
+            new Category{Name="CSC 435"},
             new Category{Name="Network Security"},
             new Category{Name="Criminal Justice"}
+            
             };
             foreach (Category c in categories)
             {
@@ -43,9 +45,10 @@ namespace FinalAssignment7.Data
 
             var notes = new Note[]
             {
-                new Note{Title="Note 1",Notes="How much did I learn",CreatedOn=DateTime.Parse("2018-02-13"),CategoryId=1,UserId=1,IsDeleted=false },
-                new Note{Title="Note 2",Notes="This is was hard",CreatedOn=DateTime.Parse("2018-03-27"),CategoryId=2,UserId=2,IsDeleted=false },
-                new Note{Title="Note 3",Notes="Im glad its finals time",CreatedOn=DateTime.Parse("2018-2-11"),CategoryId=3,UserId=3,IsDeleted=false }
+                new Note{Title="Note 1",Notes="Git - Create Account, Set up SSH key, Create / Clone Repository.",CreatedOn=DateTime.Parse("2018-02-13"),CategoryId=1,UserId=1,IsDeleted=false },
+                new Note{Title="Note 2",Notes="Git - fetch, pull, branch, checkout.",CreatedOn=DateTime.Parse("2018-03-27"),CategoryId=2,UserId=2,IsDeleted=false },
+                new Note{Title="Note 3",Notes="Visual Studio - Create web application create Service, Domain, Repository, Core Services.",CreatedOn=DateTime.Parse("2018-2-11"),CategoryId=3,UserId=3,IsDeleted=false }
+                
             };
             foreach (Note n in notes)
             {
